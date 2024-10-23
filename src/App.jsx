200d3b27e0bbae0b93c734eb66ada3b42ad2063d
pangphaichit <pg_grj@hotmail.com>
2024-10-23 14:34:25 +0700

    feat: add input field for greeting message

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingMessage, setGreetingMessage] = useState("");
+  const [inputMessage, setInputMessage] = useState("");
+
+const messageInputHandler = (event) => {
+  setInputMessage(event.target.value)
+};
+
+const updateTextButtonHandler = () => {
+  setGreetingMessage(inputMessage)
+};
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage ? greetingMessage : "Greeting Message"}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input id="greeting-message" type="text"value={inputMessage} onChange={messageInputHandler}/>
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={updateTextButtonHandler}>Update text</button>
       </div>
     </div>
   );
